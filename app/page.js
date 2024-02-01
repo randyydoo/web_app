8116b78b6d42b676401909cb804e7fc20b465bad
randyydoo <randyydoo@gmail.com>
2024-01-31 22:08:27 -0800

    added container to hero page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import { Container } from "postcss";
 import Bio from "./components/Bio";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-black">
-      <Bio />
-      </main>
+    <main className="flex min-h-screen flex-col bg-black container mx-auto px-12 py-4">
+      <div className="container">
+        <Bio />
+      </div>
+    
+    </main>
   );
 }
