6cc38f392995b8e2b395e47b9c29274708631e73
randyydoo <randyydoo@gmail.com>
2024-02-01 10:54:21 -0800

    added button styling

--- app/components/Bio.jsx
+++ app/components/Bio.jsx
@@ -2,18 +2,18 @@
 import React from 'react'
 import Image from 'next/image';
 import { TypeAnimation } from 'react-type-animation';
-
-
-
-
-
-
+import { motion } from "framer-motion";
 
 const bio = () => {
     return (
         <section>
             <div className='grid grid-cols-1 sm:grid-col-12'>
-                <div className='col-span-7 place-self-center text-center sm:text-left'>
+                <motion.div
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.7 }}
+                    className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+                >
                     <h1 className="text-green-500 mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
                         <TypeAnimation
                             sequence={[
@@ -21,7 +21,7 @@ const bio = () => {
                                 "Hello World, I'm Randy Do",
                                 1000,
                                 "Hello World, I love problem solving",
-                                1000           
+                                1000
                             ]}
                             wrapper="span"
                             speed={50}
@@ -34,26 +34,30 @@ const bio = () => {
                         problem solving, AI/ML, and web development.
                     </p>
                     <div>
-                        <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-green-500 hover:bg-black text-white border border-white font-extrabold mt-3">
+                        <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-green-500 hover:text-green-500 hover:bg-black hover:border-green-500 text-white border border-white font-extrabold mt-3">
                             Hire Me
                         </button>
-                        <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-green-500 hover:bg-black text-white border border-white font-extrabold mt-3">
+                        <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-green-500  hover:text-green-500 hover:bg-black hover:border-green-500 text-white border border-white font-extrabold mt-3">
                             Download Resume
                         </button>
                     </div>
-                </div>
-                <div className="col-span-4 place-self-center mt-4 lg:mt-0">
-                    <div className="col-span-5 place-self-center w-[300px] h-[300px] lg:w-[425px] lg:h-[500px] mt-4 lg:mt-0">
+                </motion.div>
+                <motion.div
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.7 }}
+                    className="col-span-4 place-self-center mt-4 lg:mt-0"
+                >
+                    <div className="rounded full col-span-5 place-self-center w-[300px] h-[300px] lg:w-[425px] lg:h-[500px] mt-4 lg:mt-0 relative">
                         <Image
                             src="/images/me.png"
                             alt='me image'
                             className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
-                            style=""
                             width={300}
                             height={300}
                         />
                     </div>
-                </div>
+                </motion.div>
             </div>
         </section>
     );
